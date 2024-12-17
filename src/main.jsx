c091760b2613b7a229e2c9a8e0eb77bde84dff63
emeliege <ibeokoyefranklin@gmail.com>
2024-12-17 15:38:02 +0100

    master

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { FilterProvider } from './contexts/FilterContext';
+import { FilterProvider } from './Contexts/FilterContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <FilterProvider>
